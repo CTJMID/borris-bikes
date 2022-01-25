fa9c9c643cd7d7310c0882c7bf7424fe7d323f26
Chris Middleton <ctjmiddleton@gmail.com>
2022-01-25 15:57:17 +0000

    Docking Station Class & Test

---
+++ lib/docking_station.rb
@@ -0,0 +1,4 @@
+class DockingStation
+    def release(bike)
+    end
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/docking_station.rb'
+describe DockingStation do 
+end
+   
+   
+   
+   
+
+
+   
+   
+    # describe '#bike' dd
+    #   it 'should release a bike' do
+    #     docking_station = DockingStation.new
+    #     expect (docking_station).to resond_to(bike).
